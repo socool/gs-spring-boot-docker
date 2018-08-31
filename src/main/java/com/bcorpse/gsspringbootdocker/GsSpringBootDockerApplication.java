4d5ce21a794cf9b1a5e35d8eef96667226d07d31
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-08-31 23:13:22 +0700

    add rest controller hello

--- src/main/java/com/bcorpse/gsspringbootdocker/GsSpringBootDockerApplication.java
+++ src/main/java/com/bcorpse/gsspringbootdocker/GsSpringBootDockerApplication.java
@@ -2,9 +2,16 @@ package com.bcorpse.gsspringbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GsSpringBootDockerApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsSpringBootDockerApplication.class, args);
